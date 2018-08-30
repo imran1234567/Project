948c37e2fb9fceb09d257f03033862465a6876e1
imran1234567 <imran@kgec.edu.in>
2018-08-30 17:14:17 +0530

    Initial Changes for my first project

---
+++ src/main/java/com/devopsimran/TalkinApplication.java
@@ -0,0 +1,12 @@
+package com.devopsimran;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TalkinApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TalkinApplication.class, args);
+	}
+}
